39ce8b5da01035753dcc3508abdaa48f01c2fbfd
TNolan01 <ihavesevenfingers@gmail.com>
2022-07-25 14:24:55 +0000

    Setup Heroku App and connect Postgres DB and test link

--- tommy1/settings.py
+++ tommy1/settings.py
@@ -3,6 +3,9 @@
 from pathlib import Path
 from dotenv import load_dotenv
 import os
+import dj_database_url
+if os.path.isfile('env.py'):
+    import env
 
 load_dotenv()
 
@@ -10,17 +13,14 @@ load_dotenv()
 BASE_DIR = Path(__file__).resolve().parent.parent
 
 
-# Quick-start development settings - unsuitable for production
-# See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
-
-# SECURITY WARNING: keep the secret key used in production secret!
 SECRET_KEY = os.getenv('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+X_FRAME_OPTIONS = 'SAMEORIGIN'
 
+ALLOWED_HOSTS = ['tw-shop.herokuapp.com', 'localhost']
 
 # Application definition
 
@@ -93,11 +93,15 @@ WSGI_APPLICATION = 'tommy1.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/3.2/ref/settings/#databases
 
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': BASE_DIR / 'db.sqlite3',
+#     }
+# }
+
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
+    'default': dj_database_url.parse(os.environ.get('DATABASE_URL'))
 }
 
 
