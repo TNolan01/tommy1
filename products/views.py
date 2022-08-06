26aadcb0900b1b921bc8735f4e091116ae8515db
TNolan01 <ihavesevenfingers@gmail.com>
2022-08-06 12:59:30 +0000

    Created product detail page. Styled carousel

--- products/views.py
+++ products/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 from .models import Product
 
 # Renders the all products page
@@ -9,3 +9,13 @@ def all_products(request):
     }
     
     return render(request, 'products/products.html', context)
+
+
+# Renders the all product DETAIL page
+def product_detail(request, product_id):
+    product = get_object_or_404(Product, pk=product_id)    
+    context = {
+        'product': product,
+    }
+    
+    return render(request, 'products/product_detail.html', context)
