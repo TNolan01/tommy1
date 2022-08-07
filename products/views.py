f7e96074a48e9019f21accd87aba1f32770ccce2
TNolan01 <ihavesevenfingers@gmail.com>
2022-08-07 11:45:00 +0000

    Create function for nav-bar search and to sort products into categories

--- products/views.py
+++ products/views.py
@@ -1,12 +1,35 @@
-from django.shortcuts import render, get_object_or_404
-from .models import Product
+from django.shortcuts import render, reverse, redirect, get_object_or_404
+from django.contrib import messages
+from django.db.models import Q
+from .models import Product, Category
 
 # Renders the all products page
 def all_products(request):
     products = Product.objects.all()
+    query = None
+    categories = None
+    if request.GET:
+        # filter to show products in their catergories
+        if 'category' in request.GET:
+            categories = request.GET['category'].split(',')
+            products = products.filter(category__name__in=categories)
+            categories = Category.objects.filter(name__in=categories)
+            
+        #query for nav search function    
+        if 'q' in request.GET:
+            query = request.GET['q']
+            if not query:
+                messages.error(request,"Please enter search query.")
+                return redirect(reverse('products'))
+            
+            queries = Q(product_name__icontains=query) | Q(product_description__icontains=query)
+            products = products.filter(queries)    
+    
     context = {
         'products': products,
-    }
+        'search_func': query,
+        'product_cats': categories,
+    }      
     
     return render(request, 'products/products.html', context)
 
