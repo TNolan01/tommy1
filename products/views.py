fa5d08f95e4793d1a6b9d15d0ce46ceaa84d0244
TNolan01 <ihavesevenfingers@gmail.com>
2022-08-10 22:04:47 +0000

    Created product sort feature with JS

--- products/views.py
+++ products/views.py
@@ -1,19 +1,41 @@
 from django.shortcuts import render, reverse, redirect, get_object_or_404
 from django.contrib import messages
-from django.db.models import Q
+from django.db.models import Q  # For search function
+from django.db.models.functions import Lower # For product sort via name
 from .models import Product, Category
+
 
 # Renders the all products page
 def all_products(request):
     products = Product.objects.all()
     query = None
     categories = None
+    sort = None
+    direction = None
+    
     if request.GET:
+        # product sorting function
+        if 'sort' in request.GET:
+            sortkey = request.GET['sort']
+            sort = sortkey
+            if sortkey == 'product_name':
+                sortkey = 'lower_name'
+                products = products.annotate(lower_name=Lower('product_name'))
+            if sortkey == 'category':
+                sortkey = 'category__name'
+            if 'direction' in request.GET:
+                direction = request.GET['direction']
+                if direction == 'desc':
+                    sortkey = f'-{sortkey}'
+            products = products.order_by(sortkey)
+            
+            
         # filter to show products in their catergories
         if 'category' in request.GET:
             categories = request.GET['category'].split(',')
             products = products.filter(category__name__in=categories)
             categories = Category.objects.filter(name__in=categories)
+            
             
         #query for nav search function    
         if 'q' in request.GET:
@@ -25,16 +47,19 @@ def all_products(request):
             queries = Q(product_name__icontains=query) | Q(product_description__icontains=query)
             products = products.filter(queries)    
     
+    current_sorting = f'{sort}_{direction}'
+    
     context = {
         'products': products,
         'search_func': query,
         'product_cats': categories,
+        'current_sorting': current_sorting,
     }      
     
     return render(request, 'products/products.html', context)
 
 
-# Renders the all product DETAIL page
+# Renders the product DETAIL page
 def product_detail(request, product_id):
     product = get_object_or_404(Product, pk=product_id)    
     context = {
