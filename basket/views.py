f80e749125558bf8c6f6dadb7fd3e51d16270ba7
TNolan01 <ihavesevenfingers@gmail.com>
2022-08-14 13:11:02 +0000

    Created add and remove function in basket and add the quantity function to product detail

--- basket/views.py
+++ basket/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render, redirect
+from django.shortcuts import render, redirect, reverse, HttpResponse
 
 # Renders shopping basket
 
@@ -18,4 +18,30 @@ def add_to_basket(request, item_id):
     
     request.session['basket'] = basket
     return redirect(redirect_url)
+
+
+# Adjust quantity of a product in the basket
+def adjust_basket(request, item_id):
+    quantity = int(request.POST.get('quantity'))
     
+    
+    if quantity > 0:
+        basket[item_id] = quantity
+    else:
+        basket.pop[item_id]
+
+    request.session['basket'] = basket
+    return redirect(reverse('view_basket'))
+
+
+# Remove a product from the basket
+def remove_from_basket(request, item_id):
+    basket = request.session.get('basket',{})
+    try:
+        basket.pop(item_id)
+        
+        request.session['basket'] = basket
+        return HttpResponse(status=200)
+    
+    except Exception as e:
+        return HttpResponse(status=500)
