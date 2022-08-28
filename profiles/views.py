480df2ec1e857da8324f68e80dfa88ff4d9039cd
TNolan01 <ihavesevenfingers@gmail.com>
2022-08-28 11:48:55 +0000

    Created profile html template

--- profiles/views.py
+++ profiles/views.py
@@ -1,7 +1,16 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
+from .models import UserProfile
+from .forms import UserProfileForm
 
 def profile(request):
+    profile = get_object_or_404(UserProfile, user=request.user)
+    form = UserProfileForm(instance=profile)
+    orders = profile.orders.all()
     template = 'profiles/profile.html'
-    context ={}
+    context ={
+        'profile' : profile,
+        'form': form,
+        'orders': orders,
+    }
 
     return render(request,template,context)
