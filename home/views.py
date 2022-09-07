1b5d548d0f8bfdce0b6bbbd9838cd19b91d7cc05
TNolan01 <ihavesevenfingers@gmail.com>
2022-09-07 21:50:40 +0000

    Work on contact us page, created Base.SCSS

--- home/views.py
+++ home/views.py
@@ -2,6 +2,8 @@ from django.shortcuts import render, redirect
 from marketing.forms import CustomerForm
 from django.contrib import messages
 from django.conf import settings
+from django.core.mail import send_mail, BadHeaderError
+from django.http import HttpResponse, HttpResponseRedirect
 from .forms import ContactForm
 
 
@@ -12,17 +14,25 @@ def index(request):
 
 # Renders contact us page
 def contact_us(request):
-    if request.method == 'POST':
+    if request.method == 'GET':
+        form = ContactForm()
+    else:
         form = ContactForm(request.POST)
         if form.is_valid():
-            form.save()
-            messages.success(request, 'We will be in contact with you shortly') 
+            contact_name = form.cleaned_data["contact_name"]
+            from_email = form.cleaned_data["email"]
+            phone_number = form.cleaned_data['phone_number']
+            subject = contact_name + ' ' + phone_number
+            message = form.cleaned_data['message']
+            recipient_list = ['sales@thissite.com']
+            try:
+                # send_mail(contact_name, email, phone_number, message, ['sales@site.ie'])
+                send_mail(subject, message, from_email, recipient_list, fail_silently=False, auth_user=None, auth_password=None, connection=None, html_message=None)
+            except BadHeaderError:
+                return HttpResponse("Invalid header found.")
+            messages.success(request,'We will be in contact with you shortly')
             return redirect('/')
-    else:
-        form = ContactForm()
-    context = {'form': form}
-
-    return render(request, 'home/contact_us.html', context)
+    return render(request, 'home/contact_us.html', {'form': form})
 
 
 def subscribe(request):
@@ -30,7 +40,7 @@ def subscribe(request):
         form = CustomerForm(request.POST)
         if form.is_valid():
             form.save()
-            messages.success(request, 'We will be in contact with you shortly') 
+            messages.success(request, 'Thank you for signing up!') 
             return redirect('/')
     else:
         form = CustomerForm()
