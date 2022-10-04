a69611009f9bf324a25d4b0520a853f603fd3a95
TNolan01 <ihavesevenfingers@gmail.com>
2022-10-04 22:33:30 +0000

    AWS changes for testing

--- tommy1/settings.py
+++ tommy1/settings.py
@@ -171,9 +171,8 @@ if 'USE_AWS' in os.environ:
     # AWS Bucket configuration
     AWS_STORAGE_BUCKET_NAME = 'tw-shop'
     AWS_S3_REGION_NAME = 'eu-west-1'
-    AWS_ACCESS_KEY_ID = os.environ.get('AWS_ACCESS_KEY_ID')
-    AWS_SECRET_ACCESS_KEY_ID = os.environ.get('AWS_SECRET_ACCESS_KEY')
-    # AWS_SECRET_ACCESS_KEY_ID = os.environ.get('AWS_SECRET_ACCESS_KEY').strip("\n")
+    AWS_ACCESS_KEY_ID = os.environ.get('AWS_ACCESS_KEY_ID').strip("\n")
+    AWS_SECRET_ACCESS_KEY_ID = os.environ.get('AWS_SECRET_ACCESS_KEY').strip("\n")
     AWS_S3_CUSTOM_DOMAIN = f'{AWS_STORAGE_BUCKET_NAME}.s3.amazonaws.com'
     S3_USE_SIGV4 = True
     AWS_S3_SIGNATURE_VERSION = "s3v4" 
