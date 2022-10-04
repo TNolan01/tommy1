63ae613548371f25fb3a54e01d335be19962d0ea
TNolan01 <ihavesevenfingers@gmail.com>
2022-10-04 23:50:35 +0000

    Change env.py

--- tommy1/settings.py
+++ tommy1/settings.py
@@ -173,8 +173,8 @@ if 'USE_AWS' in os.environ:
     AWS_S3_REGION_NAME = 'eu-west-1'
     # AWS_ACCESS_KEY_ID = os.environ.get('AWS_ACCESS_KEY_ID').strip("\n")
     # AWS_SECRET_ACCESS_KEY_ID = os.environ.get('AWS_SECRET_ACCESS_KEY').strip("\n")
-    AWS_ACCESS_KEY_ID = os.getenv('AWS_ACCESS_KEY_ID').strip("\n")
-    AWS_SECRET_ACCESS_KEY_ID = os.getenv('AWS_SECRET_ACCESS_KEY_ID').strip("\n")
+    AWS_ACCESS_KEY_ID = os.getenv('AWS_ACCESS_KEY_ID')
+    AWS_SECRET_ACCESS_KEY_ID = os.getenv('AWS_SECRET_ACCESS_KEY_ID')
     AWS_S3_CUSTOM_DOMAIN = f'{AWS_STORAGE_BUCKET_NAME}.s3.amazonaws.com'
     AWS_S3_SIGNATURE_VERSION = 's3v4'
         
