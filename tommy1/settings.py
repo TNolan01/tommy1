1734c70ec544b92450a106349fd51e02b2c45b6d
TNolan01 <ihavesevenfingers@gmail.com>
2022-10-04 23:42:11 +0000

    Changed AWS keys to env file

--- tommy1/settings.py
+++ tommy1/settings.py
@@ -171,13 +171,12 @@ if 'USE_AWS' in os.environ:
     # AWS Bucket configuration
     AWS_STORAGE_BUCKET_NAME = 'tw-shop'
     AWS_S3_REGION_NAME = 'eu-west-1'
-    AWS_ACCESS_KEY_ID = os.environ.get('AWS_ACCESS_KEY_ID').strip("\n")
-    AWS_SECRET_ACCESS_KEY_ID = os.environ.get('AWS_SECRET_ACCESS_KEY').strip("\n")
+    # AWS_ACCESS_KEY_ID = os.environ.get('AWS_ACCESS_KEY_ID').strip("\n")
+    # AWS_SECRET_ACCESS_KEY_ID = os.environ.get('AWS_SECRET_ACCESS_KEY').strip("\n")
+    AWS_ACCESS_KEY_ID = os.getenv('AWS_ACCESS_KEY_ID')
+    AWS_SECRET_ACCESS_KEY_ID = os.getenv('AWS_SECRET_ACCESS_KEY_ID')
     AWS_S3_CUSTOM_DOMAIN = f'{AWS_STORAGE_BUCKET_NAME}.s3.amazonaws.com'
-    S3_USE_SIGV4 = True
-    AWS_S3_SIGNATURE_VERSION = "s3v4" 
-
-    
+        
     #Storage of Static and Media Files
     STATICFILES_STORAGE = 'custom_storages.StaticStorage'
     STATICFILES_LOCATION = 'static'
